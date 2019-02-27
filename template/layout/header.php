3ca425fb9c189b7c4da8549aa2bda9af40fdf592
Student <Student@rubiconepro.ru>
2019-02-27 20:59:55 +0300

    do index and add css js

--- template/layout/header.php
+++ template/layout/header.php
@@ -1,4 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="/css/site.css" />
+    <script src="/js/jquery-3.3.1.js"></script>
+    <script src="/js/bootstrap.bundle.js"></script>
+</head>
+<body>
+
+    <header class="blog-header py-3 headerwight">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-4 pt-1">
+        <a class="text-muted" href="#">Subscribe</a>
+      </div>
+      <div class="col-4 text-center">
+        <a class="blog-header-logo text-dark" href="#">Large</a>
+      </div>
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <a class="text-muted" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+        </a>
+        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+      </div>
+    </div>
+  </header>
+
 thit is heder
-<div class="newclass" style="text-align:center;">
+<div class="header-logo" style="text-align:center;">
     <img src="/img/toplogo.jpg" />
  </div>
--- template/layout/index.php
+++ template/layout/index.php
@@ -1,12 +1,11 @@
-
 <?= $this->WriteHTML("", "layout", "header") ?>
 
-<ul class="nav nav-pills nav-fill justify-content-center inline">
+<ul class="nav nav-pills nav-fill navmy">
   <li class="nav-item">
     <a class="nav-link active" href="#">active</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link active" href="#">Главная</a>
+    <a class="nav-link" href="#">Главная</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Магазин</a>
@@ -37,21 +36,68 @@
   this id index
   <div class="container">
     <div class="row">
+      <div class="line-empty"></div>
+    </div>
+    <div class="row">
       <div class="col-sm">
-        Одна из трёх колонок
+        <div class="card" style="width: 18rem;">
+           <img src="..." class="card-img-top" alt="...">
+           <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           </div>
+        </div>
       </div>
       <div class="col-sm">
-        Одна из трёх колонок
+        <div class="card" style="width: 18rem;">
+           <div class="element indent">
+              <div class="element-post-title">
+                <p class="element-text-title"> Наши новинки </p>
+              </div>
+              <div class="element-post-text">
+                 <p class="element-text"> новая коллекция </p>
+              </div>
+              <div class="element-post-button">
+                 <a class="button-view-1" href="http://flavourknit.ru/shop/"> перейти в магазин </a>
+               <button type="button" class="btn btn-outline-secondary">перейти в магазин2</button>
+           </div>
+        </div>
       </div>
       <div class="col-sm">
-        Одна из трёх колонок
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+    <div class="line-empty"></div>
+  </div>
+    <div class="row">
+      <div class="col-sm">
+        Два Одна из трёх колонок
+      </div>
+      <div class="col-sm">
+       Два  Одна из трёх колонок
+      </div>
+      <div class="col-sm">
+        Два Одна из трёх колонок
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        три Одна из трёх колонок
+      </div>
+      <div class="col-sm">
+       Три  Одна из трёх колонок
+      </div>
+      <div class="col-sm">
+        Три Одна из трёх колонок
       </div>
     </div>
   </div>
 </div>
-
-<?= $this->WriteHTML("", "layout", "footer") ?>
-
 
 <script>
 $('#myTab a').click(function (e) {
@@ -61,3 +107,6 @@ $('#myTab a').click(function (e) {
 
 
 </script>
+
+
+<?= $this->WriteHTML("", "layout", "footer") ?>
