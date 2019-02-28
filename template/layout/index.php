58ea66b4160ff00b5264e7c17e87b351f2a2b468
Student <Student@rubiconepro.ru>
2019-02-28 21:06:22 +0300

    do main page

--- template/layout/index.php
+++ template/layout/index.php
@@ -32,70 +32,183 @@
     <a class="nav-link disabled" href="#">Disabled</a>
   </li>
 </ul>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
 <div class= "main-content">
-  this id index
+  this is index
   <div class="container">
     <div class="row">
       <div class="line-empty"></div>
     </div>
     <div class="row">
+      <div class="line-decorate"></div>
+    </div>
+    <div class="row">
       <div class="col-sm">
-        <div class="card" style="width: 18rem;">
-           <img src="..." class="card-img-top" alt="...">
+        <div class="card" style="width: 20rem;">
+           <img src="/img/images1.jpg" class="card-img-top" alt="...">
            <div class="card-body">
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
            </div>
         </div>
       </div>
       <div class="col-sm">
-        <div class="card" style="width: 18rem;">
-           <div class="element indent">
+        <div class="card" style="width: 20rem;">
+              <div class="element-indent-empty">
+              </div>
               <div class="element-post-title">
-                <p class="element-text-title"> Наши новинки </p>
+                <p class="text-center text-uppercase font-weight-bold"> Наши новинки </p>
+              </div>
+              <div class="element-line-decorate">
+                  <img src="/img/line-decor1.png" class="card-img-top" alt="..." >
               </div>
               <div class="element-post-text">
-                 <p class="element-text"> новая коллекция </p>
+                 <p class="text-center font-italic"> новая коллекция </p>
+              </div>
+              <div class="element-indent-empty">
               </div>
               <div class="element-post-button">
-                 <a class="button-view-1" href="http://flavourknit.ru/shop/"> перейти в магазин </a>
-               <button type="button" class="btn btn-outline-secondary">перейти в магазин2</button>
+                  <a class="button-view-1" href="http://"> перейти в магазин </a>
+                  <button type="button" class="btn btn-outline-secondary">перейти в магазин2</button>
+              </div>
+            
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 20rem;">
+            <img src="/img/images2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+      </div>
+    </div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+    <div class="row">
+      <div class="line-empty"></div>
+    </div>
+    <div class="row">
+      <div class="line-decorate"></div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <div class="card" style="width: 20rem;">
+           <img src="/img/images3.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
            </div>
         </div>
       </div>
       <div class="col-sm">
-        <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div class="card" style="width: 20rem;">
+           <div class="element indent">
+              <div class="element-post-title">
+                <p class="element-text-title"> Наши акции </p>
+              </div>
+              <div class="element-post-text">
+                 <p class="element-text"> новые акции </p>
+              </div>
+              <div class="element-post-button">
+                  <a class="button-view-1" href="http://"> перейти в магазин </a>
+                  <button type="button" class="btn btn-outline-secondary">перейти в магазин2</button>
+              </div>
+            </div>
         </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 20rem;">
+            <img src="/img/images4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
         </div>
       </div>
     </div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
     <div class="row">
-    <div class="line-empty"></div>
+      <div class="line-empty"></div>
+    </div>
+    <div class="row">
+      <div class="line-decorate"></div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <div class="card" style="width: 20rem;">
+           <img src="/img/images5.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 20rem;">
+           <img src="/img/images7.jpg" class="card-img-top" alt="...">
+           <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           </div>
+        </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 20rem;">
+            <img src="/img/images6.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+      </div>
+    </div>
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+      
+
   </div>
-    <div class="row">
-      <div class="col-sm">
-        Два Одна из трёх колонок
-      </div>
-      <div class="col-sm">
-       Два  Одна из трёх колонок
-      </div>
-      <div class="col-sm">
-        Два Одна из трёх колонок
-      </div>
+  <div class="section-after-content">
+    <div class="container">
+    <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+        <div class="row">
+          <div class="col col-md-24">.col .col-md-24</div>
+        
+        </div>
+        <div class="row">
+          <div class="col-24 col-md-4">.col .col-md-4</div>
+          <div class="col-24 col-md-7">.col .col-md-7</div>
+          <div class="col-24 col-md-2">.col .col-md-2</div>
+          <div class="col-24 col-md-4">.col .col-md-4</div>
+          <div class="col-24 col-md-7">.col .col-md-7</div>
+        </div>
+
+        <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+        <div class="row">
+          <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+          <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+          <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+        </div>
+
+        <!-- Columns are always 50% wide, on mobile and desktop -->
+        <div class="row">
+          <div class="col-6">.col-6</div>
+          <div class="col-6">.col-6</div>
+        </div>
     </div>
-    <div class="row">
-      <div class="col-sm">
-        три Одна из трёх колонок
-      </div>
-      <div class="col-sm">
-       Три  Одна из трёх колонок
-      </div>
-      <div class="col-sm">
-        Три Одна из трёх колонок
-      </div>
-    </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">col</div>
+          <div class="col">col</div>
+          <div class="col">col</div>
+          <div class="col">col</div>
+          <div class="col">col</div>
+        </div>
+        <div class="row">
+          <div class="col-8">col-8</div>
+          <div class="col-4">col-4</div>
+        </div>
+</div>
+
+
+
   </div>
 </div>
 
