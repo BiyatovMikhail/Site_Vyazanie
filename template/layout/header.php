8a452743ee94f9b933e1fa0d4aa266044cb98581
Unknown <mixail_956@rambler.ru>
2019-05-27 16:58:35 +0300

    do header

--- template/layout/header.php
+++ template/layout/header.php
@@ -25,8 +25,8 @@
 <body>
 
     <header class="blog-header py-3 headerwight">
-    <div class="row flex-nowrap justify-content-between align-items-center">
-      <div class="col-4 pt-1">
+    <div class="row ">
+      <div class="col-4 ">
         <a class="text-muted" href="#">Subscribe</a>
       </div>
       <div class="col-4 text-center">
--- template/loginform/logoutform.php
+++ template/loginform/logoutform.php
@@ -2,16 +2,26 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" focusable="false" role="img"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
 </a>
 
-<div class="dropdown">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-  <p><?= $MODEL ?></p>
-  </a>
 
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+<div class="btn-group">
+  <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <?= $MODEL ?>
+  </button>
+  <div class="dropdown-menu">
+  <a class="dropdown-item" href="/user1/showprofileuser">профиль/</a>
+      <a class="dropdown-item" href="/order/showorderuser">мои_заказы/</a>
+      <a class="dropdown-item" href="/user1/orderuserAll">мои_заказы2/</a>
+      <a class="dropdown-item" href="/user1/basketUser">корзина/</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="/admin">Админка</a>
+      <div class="dropdown-divider"></div>
     <a class="dropdown-item" href="#">Action</a>
     <a class="dropdown-item" href="#">Another action</a>
     <a class="dropdown-item" href="#">Something else here</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Separated link</a>
   </div>
 </div>
+
 <p> / </p>
 <a class="btn btn-sm btn-outline-secondary" href="/loginform/logOut">Выйти</a>
